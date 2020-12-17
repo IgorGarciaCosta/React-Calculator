007f32852c0fa29a3573b51f33416a80e6f5c4dd
IgorGarciaCosta <igonildo7@gmail.com>
2020-12-17 12:11:00 -0300

    botão de limpar funcionando, sinal de ponto e condições pra add o 0

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react'
 import './App.css';
 import Button from './components/Button';
 import Input from './components/Input'
+import ClearButton from './components/ClearButton';
 
 class App extends Component {
   constructor(props){//fiz o cosntrutor pq to usando setState
@@ -18,6 +19,24 @@ class App extends Component {
     this.setState({input: this.state.input + val});
   };
 
+  addZeroToInput = val =>{
+    if(this.state.input !== ""){//se tem algum número
+      this.setState({input:this.state.input+val});
+    }
+  }
+
+  addDecimal = val =>{
+    //só add um decimal se não houver outro ponto
+    if(this.state.input.indexOf(".")===-1){//===-1 significa que não tem tal coisa
+      this.setState({input: this.state.input + val})
+    }
+  }
+
+  clearInput = ()=>{
+    this.setState({input:""});
+  }
+
+
   render() {
     return (
       <div className="App">
@@ -30,7 +49,7 @@ class App extends Component {
             <Button>x^2</Button>
             <Button>(</Button>
             <Button>)</Button>
-            <Button>C</Button>
+            <ClearButton handleClear={this.clearInput}>C</ClearButton>
             <Button>Del</Button>
           </div>
 
@@ -38,34 +57,34 @@ class App extends Component {
             <Button>π</Button>
             <Button>Sen</Button>
             <Button handleClick={this.addToInput}>7</Button>
-            <Button>8</Button>
-            <Button>9</Button>
-            <Button>÷</Button>
+            <Button handleClick={this.addToInput}>8</Button>
+            <Button handleClick={this.addToInput}>9</Button>
+            <Button >÷</Button>
           </div>
 
           <div className="row">
             <Button>e</Button>
             <Button>Cos</Button>
-            <Button>4</Button>
-            <Button>5</Button>
-            <Button>6</Button>
+            <Button handleClick={this.addToInput}>4</Button>
+            <Button handleClick={this.addToInput}>5</Button>
+            <Button handleClick={this.addToInput}>6</Button>
             <Button>*</Button>
           </div>
 
           <div className="row">
             <Button>X!</Button>
             <Button>Tan</Button>
-            <Button>1</Button>
-            <Button>2</Button>
-            <Button>3</Button>
+            <Button handleClick={this.addToInput}>1</Button>
+            <Button handleClick={this.addToInput}>2</Button>
+            <Button handleClick={this.addToInput}>3</Button>
             <Button>−</Button>
           </div>
 
           <div className="row">
             <Button>%</Button>
             <Button>Git</Button>
-            <Button>0</Button>
-            <Button>.</Button>
+            <Button handleClick={this.addZeroToInput}>0</Button>
+            <Button handleClick={this.addDecimal}>.</Button>
             <Button>=</Button>
             <Button>+</Button>
           </div>
